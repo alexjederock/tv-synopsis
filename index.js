7333175a1ffe9e4a15caf0ce62343b09591ecc85
alexjederock <60864509+alexjederock@users.noreply.github.com>
2020-04-28 22:46:45 -0400

    Made Tv-synopsis function

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ app.get('/', (request, response) =>
   return response.render('index', { showdata })
 })
 
+app.get('/season/:num', (request, response) => {
+  const { num } = request.params
+  const season = showdata.seasons.find(season => parseInt(num) === season.number)
+
+  return response.render('season', { season })
+})
+
 app.all('*', (request, response) =>
 {
   return response.sendStatus(404)
